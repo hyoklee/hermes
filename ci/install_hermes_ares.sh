a8d340bfcf0e9b03a8a62ca1b335d248753ebf5f
H. Joe Lee <hyoklee@hdfgroup.org>
2022-04-18 22:42:55 -0500

    Update Ares script for GLPK.

--- ci/install_hermes_ares.sh
+++ ci/install_hermes_ares.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
-rm -rf ~/.spack
-rm -rf /tmp/${USER}
-git clone https://github.com/hyoklee/spack
+# rm -rf ~/.spack
+# rm -rf /tmp/${USER}
+# rm -rf ~/spack/
+# git clone https://github.com/hyoklee/spack
 # git clone https://github.com/mochi-hpc/mochi-spack-packages
-git clone https://github.com/hyoklee/hermes
+# git clone https://github.com/hyoklee/hermes
 . ./spack/share/spack/setup-env.sh
-spack compiler find
-spack repo add mochi-spack-packages
+module load cmake
+module swap openmpi3 mpich/3.2.1
+spack external find autoconf
+spack external find automake
+spack external find berkeley-db
+spack external find cmake
+spack external find diffutils
+spack external find libtool
+spack external find m4
+spack external find mpich
+spack external find ncurses
+spack external find xz
+spack install mpi ^mpich
+spack load mpi
+# spack repo add mochi-spack-packages
 spack repo add hermes/ci/hermes
-spack install ior+hermes+hdf5
+# spack spec ior+hermes+hdf5 ^abseil-cpp@20200225.2 ^mpi ^mpich ^hermes@master
+spack spec ior+hermes+hdf5  ^mpi ^mpich ^hermes@master
+# spack install ior+hermes+hdf5 ^abseil-cpp@20200225.2 ^mpi ^mpich ^hermes@master
+spack install ior+hermes+hdf5 ^mpi ^mpich ^hermes@master
 spack load ior+hermes+hdf5
 ior
 ior -a MPIIO
 ior -a HDF5
 spack view --verbose symlink install ior+hermes+hdf5
-./hermes/ci/test_ior_mpiio.sh
+./hermes/ci/test_ior_mpi.sh
 ./hermes/ci/test_ior_hdf5.sh
 
