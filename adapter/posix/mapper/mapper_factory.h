3d532630f3a27ce48441958d09a7bc5f46991d04
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2022-10-20 14:54:08 -0500

    Remove Doxygen warnings for adapter/posix/mapper/mapper_factory.h. (#270)

--- adapter/posix/mapper/mapper_factory.h
+++ adapter/posix/mapper/mapper_factory.h
@@ -19,12 +19,15 @@
 #include "singleton.h"
 
 namespace hermes::adapter::posix {
+/**
+ A class to represent mapper factory pattern
+*/
 class MapperFactory {
  public:
   /**
    * Return the instance of mapper given a type. Uses factory pattern.
    *
-   * @param type, MapperType, type of mapper to be used by the POSIX adapter.
+   * @param[in] type type of mapper to be used by the POSIX adapter.
    * @return Instance of mapper given a type.
    */
   std::shared_ptr<AbstractMapper> Get(const MapperType &type) {
