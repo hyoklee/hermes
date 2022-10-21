aa5c91fa6c8de12fe9924f9c6ecec6b0ddbd7be5
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2022-10-21 16:37:21 -0500

    Remove Doxygen warnings for src/config_parser.cc (#270)

--- src/config_parser.cc
+++ src/config_parser.cc
@@ -504,6 +504,9 @@ void ParseConfigYAML(YAML::Node &yaml_conf, Config *config) {
   CheckConstraints(config);
 }
 
+/**
+   parse YAML configuration file
+ */   
 void ParseConfig(Arena *arena, const char *path, Config *config) {
   ScopedTemporaryMemory scratch(arena);
   InitDefaultConfig(config);
