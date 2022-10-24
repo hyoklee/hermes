9bbf95631270afd4b0826d4ce352ff5efd4d2f88
hyoklee <hyoklee@users.noreply.github.com>
2022-10-24 16:29:28 +0000

    deploy: 2bb942826d0ddbe88f2eb47b456554a1fce16ece

--- html/search/functions_0.js
+++ html/search/functions_0.js
@@ -4,7 +4,7 @@ var searchData=
   ['acceptable_852',['Acceptable',['../classapi_1_1_status.html#a614badfd271231991ed2e7664b09031a',1,'api::Status']]],
   ['adapterstat_853',['AdapterStat',['../structhermes_1_1adapter_1_1mpiio_1_1_adapter_stat.html#a4b8533e96dc0b98d6cf70489769a429d',1,'hermes::adapter::mpiio::AdapterStat::AdapterStat()'],['../structhermes_1_1adapter_1_1posix_1_1_adapter_stat.html#ad34d0f241a2e93ffb19fd842b18a59e7',1,'hermes::adapter::posix::AdapterStat::AdapterStat()'],['../structhermes_1_1adapter_1_1stdio_1_1_adapter_stat.html#a1ad780e0b4a817e631a7920b26166c15',1,'hermes::adapter::stdio::AdapterStat::AdapterStat()']]],
   ['addconstraint_854',['AddConstraint',['../classhermes_1_1_linear_program.html#a9f8349d76cdfd0f88bbb8264fa0f2db8',1,'hermes::LinearProgram']]],
-  ['addschema_855',['AddSchema',['../classhermes_1_1_random.html#a4de5bdb7e5881b6531c7f110900ad4e7',1,'hermes::Random']]],
+  ['addschema_855',['AddSchema',['../classhermes_1_1_random.html#a4de5bdb7e5881b6531c7f110900ad4e7',1,'hermes::Random::AddSchema()'],['../classhermes_1_1_round_robin.html#a1e62e4415e17fa0a716113c6850b657d',1,'hermes::RoundRobin::AddSchema()']]],
   ['alignbackward_856',['AlignBackward',['../memory__management_8cc.html#a44f5b611f252c397974f44ebb7989a54',1,'hermes']]],
   ['alignforward_857',['AlignForward',['../memory__management_8cc.html#a5a5721855c1dc4e386f401611a746ad2',1,'hermes']]],
   ['appbarrier_858',['AppBarrier',['../classhermes_1_1api_1_1_hermes.html#a451f2bfb745a587e3ccfec736d992ffb',1,'hermes::api::Hermes']]],
