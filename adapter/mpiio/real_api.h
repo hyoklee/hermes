37a5b6b8497f8062698aae9be8504cc71767eb97
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2022-11-01 10:18:00 -0500

    Remove Doxygen warnings for adapter/mpiio/real_api.h. (#270)

--- adapter/mpiio/real_api.h
+++ adapter/mpiio/real_api.h
@@ -96,70 +96,101 @@ typedef int (*MPI_File_sync_t)(MPI_File fh);
 }
 
 namespace hermes::adapter::mpiio {
-
+/**
+   A class to represent MPIIO API
+*/
 class API {
  public:
+  /** MPI_Init */
   int (*MPI_Init)(int *argc, char ***argv) = nullptr;
+  /** MPI_Finalize */
   int (*MPI_Finalize)(void) = nullptr;
+  /** MPI_Wait */
   int (*MPI_Wait)(MPI_Request *req, MPI_Status *status) = nullptr;
+  /** MPI_Waitall */
   int (*MPI_Waitall)(int count, MPI_Request *req, MPI_Status *status) = nullptr;
+  /** MPI_File_open */
   int (*MPI_File_open)(MPI_Comm comm, const char *filename, int amode,
                        MPI_Info info, MPI_File *fh) = nullptr;
+  /** MPI_File_close */
   int (*MPI_File_close)(MPI_File *fh) = nullptr;
+  /** MPI_File_seek_shared */
   int (*MPI_File_seek_shared)(MPI_File fh, MPI_Offset offset,
                               int whence) = nullptr;
+  /** MPI_File_seek */
   int (*MPI_File_seek)(MPI_File fh, MPI_Offset offset, int whence) = nullptr;
+  /** MPI_File_get_position */
   int (*MPI_File_get_position)(MPI_File fh, MPI_Offset *offset) = nullptr;
+  /** MPI_File_read_all */
   int (*MPI_File_read_all)(MPI_File fh, void *buf, int count,
                            MPI_Datatype datatype, MPI_Status *status) = nullptr;
+  /** MPI_File_read_at_all */
   int (*MPI_File_read_at_all)(MPI_File fh, MPI_Offset offset, void *buf,
                               int count, MPI_Datatype datatype,
                               MPI_Status *status) = nullptr;
+  /** MPI_File_read_at */
   int (*MPI_File_read_at)(MPI_File fh, MPI_Offset offset, void *buf, int count,
                           MPI_Datatype datatype, MPI_Status *status) = nullptr;
+  /** MPI_File_read */
   int (*MPI_File_read)(MPI_File fh, void *buf, int count, MPI_Datatype datatype,
                        MPI_Status *status) = nullptr;
+  /** MPI_File_read_ordered */
   int (*MPI_File_read_ordered)(MPI_File fh, void *buf, int count,
                                MPI_Datatype datatype,
                                MPI_Status *status) = nullptr;
+  /** MPI_File_read_shared */
   int (*MPI_File_read_shared)(MPI_File fh, void *buf, int count,
                               MPI_Datatype datatype,
                               MPI_Status *status) = nullptr;
+  /** MPI_File_write_all */
   int (*MPI_File_write_all)(MPI_File fh, const void *buf, int count,
                             MPI_Datatype datatype,
                             MPI_Status *status) = nullptr;
+  /** MPI_File_write_at_all */
   int (*MPI_File_write_at_all)(MPI_File fh, MPI_Offset offset, const void *buf,
                                int count, MPI_Datatype datatype,
                                MPI_Status *status) = nullptr;
+  /** MPI_File_write_at */
   int (*MPI_File_write_at)(MPI_File fh, MPI_Offset offset, const void *buf,
                            int count, MPI_Datatype datatype,
                            MPI_Status *status) = nullptr;
+  /** MPI_File_write */
   int (*MPI_File_write)(MPI_File fh, const void *buf, int count,
                         MPI_Datatype datatype, MPI_Status *status) = nullptr;
+  /** MPI_File_write_ordered */
   int (*MPI_File_write_ordered)(MPI_File fh, const void *buf, int count,
                                 MPI_Datatype datatype,
                                 MPI_Status *status) = nullptr;
+  /** MPI_File_write_shared */
   int (*MPI_File_write_shared)(MPI_File fh, const void *buf, int count,
                                MPI_Datatype datatype,
                                MPI_Status *status) = nullptr;
+  /** MPI_File_iread_at */
   int (*MPI_File_iread_at)(MPI_File fh, MPI_Offset offset, void *buf, int count,
                            MPI_Datatype datatype,
                            MPI_Request *request) = nullptr;
+  /** MPI_File_iread */
   int (*MPI_File_iread)(MPI_File fh, void *buf, int count,
                         MPI_Datatype datatype, MPI_Request *request) = nullptr;
+  /** MPI_File_iread_shared */
   int (*MPI_File_iread_shared)(MPI_File fh, void *buf, int count,
                                MPI_Datatype datatype,
                                MPI_Request *request) = nullptr;
+  /** MPI_File_iwrite_at */
   int (*MPI_File_iwrite_at)(MPI_File fh, MPI_Offset offset, const void *buf,
                             int count, MPI_Datatype datatype,
                             MPI_Request *request) = nullptr;
+  /** MPI_File_iwrite */
   int (*MPI_File_iwrite)(MPI_File fh, const void *buf, int count,
                          MPI_Datatype datatype, MPI_Request *request) = nullptr;
+  /** MPI_File_iwrite_shared */
   int (*MPI_File_iwrite_shared)(MPI_File fh, const void *buf, int count,
                                 MPI_Datatype datatype,
                                 MPI_Request *request) = nullptr;
+  /** MPI_File_sync */
   int (*MPI_File_sync)(MPI_File fh) = nullptr;
 
+  /** API constructor that intercepts MPI API calls */
   API() {
     void *is_intercepted = (void *)dlsym(RTLD_DEFAULT, "mpiio_intercepted");
     if (is_intercepted) {
