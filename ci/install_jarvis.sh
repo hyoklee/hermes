a0f8d881cb46baad5834044416011a62ab4d6fd2
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2024-09-21 16:29:31 -0500

    ci: init jarvis

--- ci/install_jarvis.sh
+++ ci/install_jarvis.sh
@@ -2,5 +2,12 @@
 git clone https://github.com/grc-iit/jarvis-cd
 cd jarvis-cd
 pip install -e . -r requirements.txt
+# Initialize the Jarvis testing Hermes environment
+jarvis init \
+"${HOME}/jarvis-config" \
+"${HOME}/jarvis-priv" \
+"${HOME}/jarvis-shared"
+cp ../ci/resource_graph.yaml ./config/resource_graph.yaml
+jarvis env build hermes
 cd ..
 
